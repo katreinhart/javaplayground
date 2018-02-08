c36b1f2d6d9551ee35902fd8fe2ae67620f244ad
Kat Reinhart <kat.reinhart@gmail.com>
2018-02-08 15:54:05 -0800

    Initial commit

---
+++ src/main/java/com/galvanize/Application.java
@@ -0,0 +1,10 @@
+package com.galvanize;
+
+/**
+ * Created by kat on 2/8/18.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
